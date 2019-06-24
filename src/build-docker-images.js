086e0123cca565964c338e8bf208acfef858ce8d
Thanh Hoang <hoangthanh751996@gmail.com>
2019-06-24 13:37:42 +0700

    test

--- src/build-docker-images.js
+++ src/build-docker-images.js
@@ -28,7 +28,7 @@ const run = async () => {
             if (repo.name === "wi-proxy") {
                 await execShellCommand(`git clone ${repo.url}`, {cwd: "./output"});
                 await execShellCommand(`git checkout local-service`, {cwd: "./output"});
-                await execShellCommand(`git pull origin local-service`, {cwd: "./output"});
+                await execShellCommand(`git pull`, {cwd: "./output"});
                 await execShellCommand(`git merge master`, {cwd: "./output"});
                 await execShellCommand(`cp build-image.sh ../output/${repo.name}`, {cwd: "./src"});
                 if (["wi-angular", "wi-python-frontend", "base-map"].includes(repo.name)) {
