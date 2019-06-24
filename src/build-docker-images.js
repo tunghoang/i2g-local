bada62731a51efcacbf225437abff087277be214
Thanh Hoang <hoangthanh751996@gmail.com>
2019-06-24 14:09:48 +0700

    test

--- src/build-docker-images.js
+++ src/build-docker-images.js
@@ -25,22 +25,24 @@ const run = async () => {
         await execShellCommand(`ssh kubectl "rm -fr /tmp/i2g-local/*"`);
         await execShellCommand(`mkdir output`);
         await Promise.all(repos.map(async repo => {
-            await execShellCommand(`git clone ${repo.url}`, {cwd: "./output"});
-            await execShellCommand(`git checkout -b local-service`, {cwd: `./output/${repo.name}`});
-            await execShellCommand(`git pull origin local-service`, {cwd: `./output/${repo.name}`});
-            await execShellCommand(`git merge master`, {cwd: `./output/${repo.name}`});
-            await execShellCommand(`cp build-image.sh ../output/${repo.name}`, {cwd: "./src"});
-            if (["wi-angular", "wi-python-frontend", "base-map"].includes(repo.name)) {
-                if (repo.name === "base-map") {
-                    await execShellCommand(`npm i`, {cwd: `./output/${repo.name}`});
-                    await execShellCommand(`bower install`, {cwd: `./output/${repo.name}`});
-                    await execShellCommand(`bower update`, {cwd: `./output/${repo.name}`});
-                    await execShellCommand(`npm run local`, {cwd: `./output/${repo.name}`});
+            if (repo.name === "base-map") {
+                await execShellCommand(`git clone ${repo.url}`, {cwd: "./output"});
+                await execShellCommand(`git checkout -b local-service`, {cwd: `./output/${repo.name}`});
+                await execShellCommand(`git pull origin local-service`, {cwd: `./output/${repo.name}`});
+                await execShellCommand(`git merge master`, {cwd: `./output/${repo.name}`});
+                await execShellCommand(`cp build-image.sh ../output/${repo.name}`, {cwd: "./src"});
+                if (["wi-angular", "wi-python-frontend", "base-map"].includes(repo.name)) {
+                    if (repo.name === "base-map") {
+                        await execShellCommand(`npm i`, {cwd: `./output/${repo.name}`});
+                        await execShellCommand(`bower install`, {cwd: `./output/${repo.name}`});
+                        await execShellCommand(`bower update`, {cwd: `./output/${repo.name}`});
+                        await execShellCommand(`npm run local`, {cwd: `./output/${repo.name}`});
+                    }
                 }
+                await execShellCommand(`rsync --delete -azvv ./${repo.name} --rsync-path="rsync" kubectl:/tmp/i2g-local/`, {cwd: "./output"});
+                await execShellCommand(`ssh kubectl "cd /tmp/i2g-local/${repo.name} && /bin/bash build-image.sh ${REGISTRY_URL}/${repo.name}:local"`, {cwd: `./output/${repo.name}`});
+                // await execShellCommand(`rm -fr ${repo.name}`, {cwd: `./output`});
             }
-            await execShellCommand(`rsync --delete -azvv ./${repo.name} --rsync-path="rsync" kubectl:/tmp/i2g-local/`, {cwd: "./output"});
-            await execShellCommand(`ssh kubectl "cd /tmp/i2g-local/${repo.name} && /bin/bash build-image.sh ${REGISTRY_URL}/${repo.name}:local"`, {cwd: `./output/${repo.name}`});
-            // await execShellCommand(`rm -fr ${repo.name}`, {cwd: `./output`});
         }));
         // await execShellCommand(`rm -fr output`);
         // await execShellCommand(`ssh kubectl "rm -fr /tmp/i2g-local/*"`);
