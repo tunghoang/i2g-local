91e23ec49533b098f58047cbff764998a1d7ff22
Thanh Hoang <hoangthanh751996@gmail.com>
2019-06-24 14:39:03 +0700

    update build

--- src/build-docker-images.js
+++ src/build-docker-images.js
@@ -25,20 +25,24 @@ const run = async () => {
         await execShellCommand(`ssh kubectl "rm -fr /tmp/i2g-local/*"`);
         await execShellCommand(`mkdir output`);
         await Promise.all(repos.map(async repo => {
-            if (repo.name === "base-map") {
+            if (repo.name === "wi-angular") {
                 if (["wi-angular", "wi-python-frontend", "base-map"].includes(repo.name)) {
-                    await execShellCommand(`git clone ${repo.url}`, {cwd: "./output"});
-                    await execShellCommand(`git checkout -b local-service`, {cwd: `./output/${repo.name}`});
-                    await execShellCommand(`git pull origin local-service`, {cwd: `./output/${repo.name}`});
-                    await execShellCommand(`git merge master-r2`, {cwd: `./output/${repo.name}`});
-                    await execShellCommand(`cp build-image.sh ../output/${repo.name}`, {cwd: "./src"});
                     if (repo.name === "base-map") {
+                        await execShellCommand(`git clone ${repo.url}`, {cwd: "./output"});
+                        await execShellCommand(`git checkout -b local-service`, {cwd: `./output/${repo.name}`});
+                        await execShellCommand(`git pull origin local-service`, {cwd: `./output/${repo.name}`});
+                        await execShellCommand(`git merge master`, {cwd: `./output/${repo.name}`});
+                        await execShellCommand(`cp build-image.sh ../output/${repo.name}`, {cwd: "./src"});
                         await execShellCommand(`npm i`, {cwd: `./output/${repo.name}`});
                         await execShellCommand(`bower install`, {cwd: `./output/${repo.name}`});
                         await execShellCommand(`bower update`, {cwd: `./output/${repo.name}`});
                         await execShellCommand(`npm run local`, {cwd: `./output/${repo.name}`});
                     }
                     if (repo.name === "wi-angular") {
+                        await execShellCommand(`git clone ${repo.url}`, {cwd: "./output"});
+                        await execShellCommand(`git checkout -b local-service`, {cwd: `./output/${repo.name}`});
+                        await execShellCommand(`git pull origin local-service`, {cwd: `./output/${repo.name}`});
+                        await execShellCommand(`git merge master-r2`, {cwd: `./output/${repo.name}`});
                         await execShellCommand(`npm i`, {cwd: `./output/${repo.name}`});
                         await execShellCommand(`bower install`, {cwd: `./output/${repo.name}`});
                         await execShellCommand(`bower update`, {cwd: `./output/${repo.name}`});
